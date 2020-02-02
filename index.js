524d82af04d6a94c9c044e7fc2212c2e656898ae
Admin <admin@Admins-iMac.local>
2020-02-02 12:56:55 +0400

    cookie is created

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const express = require('express')
 const app = express()
+const moment = require('moment')
+const cookieParser = require('cookie-parser')
+
+app.use(cookieParser())
+
+app.use('/', (req, res, next) => {
+    console.log(req.cookie);
+
+    const cookie = req.cookies.time;
+    if (cookie === undefined) {
+        const time = moment()
+        res.cookie('time', time, { });
+        console.log('cookie created successfully');
+    }
+    else {
+        console.log('cookie exists', cookie);
+    }
+    next();
+});
+
 
 app.set('view engine', 'pug')
 app.set("views", "./views")
 
-app.get('/',(req,res)=>{
-    res.render('index',{})
+app.get('/', (req, res) => {
+    res.render('index', {
+        time: req.cookies.time
+    })
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("listen")
 })
